a25f2b7a3df242a433ebd732f21b3f1ceaa36ddc
Benjamin Yong <benj_yongzh@hotmail.com>
2023-05-08 12:04:03 +0800

    Add 2 more categories for summary.

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -8,16 +8,26 @@ const { body, validationResult } = require("express-validator");
 //display summary
 exports.summary = asyncHandler(async (req, res, next) => {
   //get details of counts of drinks and brands
-  const [drinkCount, brandCount, drinksAvailableCount] = await Promise.all([
+  const [
+    drinkCount,
+    brandCount,
+    drinkInstanceCount,
+    drinksAvailableCount,
+    drinksSoldCount,
+  ] = await Promise.all([
     Drink.countDocuments({}).exec(),
     Brand.countDocuments({}).exec(),
+    DrinkInstance.countDocuments({}).exec(),
     DrinkInstance.countDocuments({ status: "Available" }).exec(),
+    DrinkInstance.countDocuments({ status: "Sold" }).exec(),
   ]);
 
   res.render("index", {
-    title: "Drinks Inventory Application",
+    title: "Inventory",
     drink_count: drinkCount,
     brand_count: brandCount,
+    drinks_instance_count: drinkInstanceCount,
     drinks_available_count: drinksAvailableCount,
+    drinks_sold_count: drinksSoldCount,
   });
 });
