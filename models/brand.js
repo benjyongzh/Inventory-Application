4ab68acd99e78996aefa5d884d213823fa492453
Benjamin Yong <benj_yongzh@hotmail.com>
2023-05-09 12:03:31 +0800

    Start on drink_instance. Add luxon for date formatting.

--- models/brand.js
+++ models/brand.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -12,6 +13,15 @@ const BrandSchema = new Schema({
 //virtual for URL
 BrandSchema.virtual("url").get(function () {
   return "/catalog/brand/${this._id}";
+});
+
+//virtual for formatted dates
+BrandSchema.virtual("year_established_formatted").get(function () {
+  return this.year_established
+    ? DateTime.fromJSDate(this.year_established).toLocaleString(
+        DateTime.DATE_FULL
+      )
+    : "";
 });
 
 module.exports = mongoose.model("Brand", BrandSchema);
--- models/drinkinstance.js
+++ models/drinkinstance.js
@@ -20,9 +20,27 @@ DrinkInstanceSchema.virtual("url").get(function () {
   return "/catalog/drinkinstance/${this._id}";
 });
 
-//virtual for time left til expiry - find out if luxon has a duration method
-// DrinkInstanceSchema.virtual("time_left").get(function () {
-//   return "/catalog/drinkinstance/${this._id}";
-// });
+///virtual for formatted dates
+DrinkInstanceSchema.virtual("date_of_manufacture_formatted").get(function () {
+  return this.date_of_manufacture
+    ? DateTime.fromJSDate(this.date_of_manufacture).toLocaleString(
+        DateTime.DATE_FULL
+      )
+    : "";
+});
+
+DrinkInstanceSchema.virtual("date_of_expiry_formatted").get(function () {
+  return this.date_of_expiry
+    ? DateTime.fromJSDate(this.date_of_expiry).toLocaleString(
+        DateTime.DATE_FULL
+      )
+    : "";
+});
+
+DrinkInstanceSchema.virtual("date_of_sale_formatted").get(function () {
+  return this.date_of_sale
+    ? DateTime.fromJSDate(this.date_of_sale).toLocaleString(DateTime.DATE_FULL)
+    : "";
+});
 
 module.exports = mongoose.model("DrinkInstance", DrinkInstanceSchema);
