478f5283eeb03ddfc12c8909d4a8a4af7958ff2c
Benjamin Yong <benj_yongzh@hotmail.com>
2023-05-12 16:16:47 +0800

    Fix status not being set to sold when required.

--- middleware/instanceStatus.js
+++ middleware/instanceStatus.js
@@ -3,12 +3,12 @@ const { body } = require("express-validator");
 const { DateTime } = require("luxon");
 
 function setInstanceStatus(req, res, next) {
-  if (Date.now() >= req.body.date_of_expiry) {
-    //expired
-    req.body.status = "Expired";
-  } else if (body.date_of_sale instanceof Date) {
+  if (req.body.date_of_sale instanceof Date) {
     //sold
     req.body.status = "Sold";
+  } else if (Date.now() >= req.body.date_of_expiry) {
+    //expired
+    req.body.status = "Expired";
   } else {
     //not sold
     req.body.status = "Available";
